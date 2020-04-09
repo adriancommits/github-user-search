b4f62f527162ee342bc5d325408cca3dc5840c36
Adrian <mailforadrian@me.com>
2020-04-09 23:28:04 +0200

    small refactoring tasks

--- src/App.js
+++ src/App.js
@@ -27,6 +27,8 @@ class App extends Component {
   clearUsers = () => this.setState({ users: [], loading: false });
 
   render() {
+    const { users, loading } = this.state;
+
     return (
       <div className="App">
         <Navbar></Navbar>
@@ -34,9 +36,9 @@ class App extends Component {
           <Search
             searchUsers={this.searchUsers}
             clearUsers={this.clearUsers}
-            showClear={this.state.users.length > 0 ? true : false}
+            showClear={users.length > 0 ? true : false}
           />
-          <Users loading={this.state.loading} users={this.state.users}></Users>
+          <Users loading={loading} users={users}></Users>
         </div>
       </div>
     );
