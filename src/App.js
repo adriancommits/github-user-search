194454524f7a065a43575af1219449be4f47080a
Adrian <mailforadrian@me.com>
2020-04-09 22:06:55 +0200

    added search without functionality

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import "./App.css";
 import Navbar from "./components/layout/Navbar";
 import Users from "./components/users/Users";
+import Search from "./components/users/Search";
 import axios from "axios";
 
 class App extends Component {
@@ -15,8 +16,7 @@ class App extends Component {
 
     const res = await axios.get(`https://api.github.com/users?client_id=$
     {process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=$
-    {process.env.REACT_APP_GITHUB_CLIENT_SECRET}
-    `);
+    {process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
 
     this.setState({ users: res.data, loading: false });
   }
@@ -26,6 +26,7 @@ class App extends Component {
       <div className="App">
         <Navbar></Navbar>
         <div className="container">
+          <Search />
           <Users loading={this.state.loading} users={this.state.users}></Users>
         </div>
       </div>
